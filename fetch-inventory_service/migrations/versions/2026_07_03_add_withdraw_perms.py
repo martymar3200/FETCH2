f69ec096b6e52ec66d365681c6162549357fcf11
martymar3200 <martymar3200@gmail.com>
2026-07-08 17:41:12 -0400

    Please provide the code changes or file diffs you would like me to summarize.

--- fetch-inventory_service/migrations/versions/2026_07_03_add_withdraw_perms.py
+++ fetch-inventory_service/migrations/versions/2026_07_03_add_withdraw_perms.py
@@ -1,6 +1,6 @@
 """add withdraw permissions
 
-Revision ID: 2026_07_03_add_withdraw_permissions
+Revision ID: 2026_07_03_add_withdraw_perms
 Revises: 2026_05_04_scheduled_exports
 Create Date: 2026-07-03 00:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '2026_07_03_add_withdraw_permissions'
+revision: str = '2026_07_03_add_withdraw_perms'
 down_revision: Union[str, None] = '2026_05_04_scheduled_exports'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
